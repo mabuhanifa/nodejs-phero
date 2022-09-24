2437f25ab8d0ab4089c4b8583d0500bb82f5199c
shourovahmd <moh.abuhanifa@gmail.com>
2022-09-25 00:31:37 +0600

    promise.all

--- Promise.js
+++ Promise.js
@@ -4,15 +4,35 @@
 // Promises constructor for promises new Promise();
 
 const myPromise = new Promise((resolve, reject) => {
-  const user = null;
+  const user = { id: 1, email: "user@example.com" };
+
   if (!user) {
     reject("User is not available");
   } else {
     setTimeout(() => {
-      resolve("User is available");
+      resolve({ id: 1, email: "user@example.com" });
     }, 1000);
   }
 });
+
+const userIds = [1, 2, 3, 4, 5];
+
+let userData = [];
+
+// for (let i = 0; i < userIds.length; i++) {
+//   const userId = userIds[i];
+//   myPromise.then((user) => {
+//     userData.push(user);
+//   });
+
+for (let i = 0; i < userIds.length; i++) {
+  const userId = userIds[i];
+  userData.push(myPromise);
+}
+
+Promise.all(userData).then((res) => console.log(res));
+
+console.log(userData); // userData is not available until myPromise is resolved
 
 /*
  JavaScript promises have three methods. catch(), finally(), and then() .
