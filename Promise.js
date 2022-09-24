80232c28a132b9c167e1dc33f527858fb2cce150
shourovahmd <moh.abuhanifa@gmail.com>
2022-09-24 22:58:21 +0600

    promise created

--- Promise.js
+++ Promise.js
@@ -2,9 +2,19 @@
 
 // Promises constructor for promises new Promise();
 
+const myPromise = new Promise((resolve, reject) => {
+  const user = null;
+  if (!user) {
+    reject("User is not available");
+  } else {
+    setTimeout(() => {
+      resolve("User is available");
+    }, 1000);
+  }
+});
+
 /*
-
-
-
+ JavaScript promises has three methods. catch , finally, and then
 */
 
+myPromise.then((res) => console.log(res)).catch((err) => console.log(err));
