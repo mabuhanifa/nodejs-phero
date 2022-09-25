630adad532488337d1f2a43daad69955e3852f76
shourovahmd <moh.abuhanifa@gmail.com>
2022-09-26 05:00:53 +0600

    ejs

--- Module 3/index.js
+++ Module 3/index.js
@@ -6,12 +6,20 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.text());
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.use("/tools", toolsRoute);
 
+/*----------------static html----------------*/
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "./public/index.html");
+// });
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "./public/index.html");
+  res.render("home.ejs", {
+    id: 2,
+  });
 });
 
 app.all("*", (req, res) => {
