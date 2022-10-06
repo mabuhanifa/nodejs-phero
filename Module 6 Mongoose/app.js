e5f9b76d60a9a786e0a210db2f43c898c2fa4182
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-06 16:09:24 +0600

    updating

--- Module 6 Mongoose/app.js
+++ Module 6 Mongoose/app.js
@@ -6,43 +6,70 @@ const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide a name"],
-    trim: true,
-    unique: true,
-    minLength: [3, "Name must be at least 3 characters"],
-    maxLength: [100, "Name must be less than 100 characters"],
-  },
-  description: {
-    type: String,
-    required: [true, "Please provide a description"],
-  },
-  price: {
-    typeof: Number,
-    required: [true, "Please provide a price"],
-    min: [0, "Please provide a price with positive value"],
-  },
-  unit: {
-    type: String,
-    required: [true, "Please provide a unit"],
-    enum: {
-      value: ["kg", "liter", "pcs"],
-      message: "unit value cannot be {VALUE}, must be kg,liter,pcs",
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      trim: true,
+      unique: true,
+      minLength: [3, "Name must be at least 3 characters"],
+      maxLength: [100, "Name must be less than 100 characters"],
     },
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min: [0, "Please provide a quantity value"],
-    validate: {
-      validator: (value) => {
-        
+    description: {
+      type: String,
+      required: [true, "Please provide a description"],
+    },
+    price: {
+      typeof: Number,
+      required: [true, "Please provide a price"],
+      min: [0, "Please provide a price with positive value"],
+    },
+    unit: {
+      type: String,
+      required: [true, "Please provide a unit"],
+      enum: {
+        value: ["kg", "liter", "pcs"],
+        message: "unit value cannot be {VALUE}, must be kg,liter,pcs",
       },
     },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "Please provide a quantity value"],
+      validate: {
+        validator: (value) => {
+          const isInteger = Number.isInteger(value);
+          if (isInteger) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      message: "Quantity must be an integer value",
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        value: ["in-stock", "out-of-stock", "discontinued"],
+        message: "Status cannot be {VALUE}",
+      },
+    },
+    // createdAt: {
+    //   typeof: Date,
+    //   default: Date.now(),
+    // },
+    // updatedAt: {
+    //   typeof: Date,
+    //   default: Date.now(),
+    // },
   },
-});
+  {
+    timeStamps: true,
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
