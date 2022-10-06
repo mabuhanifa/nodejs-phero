28f2bbec05e4b82c24e73751c066da2d8323d222
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-06 14:15:06 +0600

    first schema

--- Module 6 Mongoose/app.js
+++ Module 6 Mongoose/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 app.use(express.json());
 app.use(cors());
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    trim: true,
+    unique: true,
+    minLength: [3, "Name must be at least 3 characters"],
+    maxLength: [100, "Name must be less than 100 characters"],
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
--- Module 6 Mongoose/server.js
+++ Module 6 Mongoose/server.js
@@ -4,12 +4,12 @@ const colors = require("colors");
 
 const app = require("./app");
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  console.log("MongoDB server connected");
+mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+  console.log("MongoDB local server connected".red.bold);
 });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log("listening on port " + port);
+  console.log("listening on port ".yellow.bold + port  );
 });
