3d4177ed0e051b8f1db9abb8e70bbd9a015be121
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-06 13:54:51 +0600

    mongodb connected

--- Module 6 Mongoose/server.js
+++ Module 6 Mongoose/server.js
@@ -3,3 +3,13 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 
 const app = require("./app");
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("MongoDB server connected");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
