25deb0e9ded3abbc8bea6147b700e2db9abf6ca5
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-06 14:27:30 +0600

    schema updating

--- Module 6 Mongoose/app.js
+++ Module 6 Mongoose/app.js
@@ -15,6 +15,33 @@ const productSchema = mongoose.Schema({
     minLength: [3, "Name must be at least 3 characters"],
     maxLength: [100, "Name must be less than 100 characters"],
   },
+  description: {
+    type: String,
+    required: [true, "Please provide a description"],
+  },
+  price: {
+    typeof: Number,
+    required: [true, "Please provide a price"],
+    min: [0, "Please provide a price with positive value"],
+  },
+  unit: {
+    type: String,
+    required: [true, "Please provide a unit"],
+    enum: {
+      value: ["kg", "liter", "pcs"],
+      message: "unit value cannot be {VALUE}, must be kg,liter,pcs",
+    },
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: [0, "Please provide a quantity value"],
+    validate: {
+      validator: (value) => {
+        
+      },
+    },
+  },
 });
 
 app.get("/", (req, res) => {
